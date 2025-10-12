41f386092a595cf0c1ab6a4478d66a84c70b6f39
Ashwin Yadav <ashwinydv2003@gmail.com>
2025-10-13 00:15:48 +0530

    Add files via upload

---
+++ CommonJs Vs EcmaScript Modules in Nodej s/main.js
@@ -0,0 +1,34 @@
+// const { createServer } = require('node:http');
+// const  fs = require('fs')
+
+// import  http from "http"
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1> Hello World </h1>');
+//   console.log("ohh yes I am run the server")
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// import {a,b,c,d} from "./mymodule.js" //it is naming export
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+import Jk from "./mymodule.js";
+ console.log(Jk)
+
+
+// const a = require("./mymodule2.js")
+
+// console.log(a, __dirname, __filename)
---
+++ CommonJs Vs EcmaScript Modules in Nodej s/mymodule.js
@@ -0,0 +1,10 @@
+export const a = 100
+export const b  = 200
+export const c = 300
+export const d = 500
+
+const Jk ={
+    x:50,
+    Y:100
+}
+export default Jk
